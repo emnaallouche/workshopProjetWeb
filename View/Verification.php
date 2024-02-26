e55d1c5de8830168ad9c56915c3fd6642f5c7adf
emnaallouche <emna.allouche@esprit.tn>
2024-02-26 20:29:42 +0100

    atelier php 5

--- View/Verification.php
+++ View/Verification.php
@@ -1,5 +1,8 @@
 <?php
 
+include '../Model/employe.php';
+include '../Controller/EmployeC.php';
+
     $fName = $_POST['fName'];
     $lName = $_POST['lName'];
     $phone = $_POST['phone'];
@@ -9,39 +12,12 @@
     $pass2 = $_POST['pass2'];
 
 
-    class EmployeC {
-        public $firstName;
-        public $lastName;
-        public $phone;
-        public $email;
-        public $dob;
-        public $password;
-
-        public function __construct($firstName, $lastName, $phone, $email, $dob, $password) {
-            $this->firstName = $firstName;
-            $this->lastName = $lastName;
-            $this->phone = $phone;
-            $this->email = $email;
-            $this->dob = $dob;
-            $this->password = $password;
-        }
-    }
-
-    function show($employe) {
-        echo "First Name: " . $employe->firstName . "<br>";
-        echo "Last Name: " . $employe->lastName . "<br>";
-        echo "Phone: " . $employe->phone . "<br>";
-        echo "Email: " . $employe->email . "<br>";
-        echo "Date of Birth: " . $employe->dob . "<br>";
-    }
-
    
-    $employe1 = new EmployeC($fName, $lName, $phone, $email, $dob, $pass1);
+    $employe1 = new Employe($fName, $lName, $phone, $email, $dob, $pass1);
+   
+var_dump($employe1);
+$employeC = new EmployeC();
+$employeC->show($employe1);
 
-    // Affichage des informations de employe1 avec var_dump()
-    var_dump($employe1);
-
-    // Affichage des informations de employe1 avec la fonction show()
-    show($employe1);
 
 ?>
